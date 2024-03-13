dd859eec4d4f365c9bd9427092999ea15f6854c6
nikoschairopoulos <nikoschairop@gmail.com>
2024-03-13 17:15:06 +0200

    First edition Create Component 4

--- InventoryApp/InventoryFrontBackEnd/Frontend/src/views/UpdateInventory.vue
+++ InventoryApp/InventoryFrontBackEnd/Frontend/src/views/UpdateInventory.vue
@@ -1,9 +1,8 @@
 <template>
-
     <div class="tablecontainer">
-    <div>
-    <div class="mb-3">
-                <label for="Choose Technology" class="form-label">Choose Technology:</label>
+    
+      <div class="mb-3">
+                <label for="ChooseTechnology" class="form-label">Choose Technology:</label>
                 <select id="ChooseTechnology" v-model="SHEET_TYPE" class="custom-select" @change="updateTypeofComponentToRender()">
                     <option value="all types">All Types</option>
                     <option value="El. Generators">Electrical Generators</option>
@@ -16,79 +15,86 @@
                     <option value="El. Storage">El. Storage</option>
                     <option value="Other">Other</option>
                 </select>
-    </div>
-    <table class="table-responsive">
+      </div>
+
+      <div class = "test">
+      <table class="table-responsive">
         <thead class="thead-dark">
-        <tr>
+          <tr>
             <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Type</th>
             <th scope="col">Subtype</th>
             <th scope="col">Actions</th>
-        </tr>
+          </tr>
         </thead>
         <tbody>
-        <tr v-for="component in componentsPerType" :key="component.id">
+          <tr v-for="component in componentsPerType" :key="component.id">
                 <td>{{ component.id }}</td>
                 <td>{{ component.name }}</td>
                 <td>{{ component.component_type }}</td>
                 <td>{{ component.component_subtype }}</td>
                 <td>
-                    <button class="btn btn-primary" style="margin-right:2px;" @click="addComponent(component)">
+                    <button class="btn btn-primary" style="margin-right:2px; background-color:green;" @click="addComponent(component)">
                         Add to Inventory
                     </button>
                 </td>
-        </tr>
+          </tr>
         </tbody>
-    </table>
-    </div>
+      </table>
 
-
-    <div class="addedcomponents" v-if="YouHaveAddComponent">
-        <h2>Sumbit Inventory Form</h2>
-        <hr>
+      <div class="addedcomponents"   v-if="YouHaveAddComponent">
         <form ref="anyName" class="container mt-4" id="component"  @submit.prevent="handleSubmit">
-            <div class="group1">
-                <div class="mb-3">
-                    <label for="name" class="form-label">Inventory Name:</label>
+          <p style=" color: black; text-align: center;">Submit Inventory Form</p>
+          <div class="group1">
+        <hr><br>
+                <div>
+                    <label for="name"  class="form-label">Demo Name:</label>
                     <input type="text" class="form-control" id="name" v-model="inventoryElement.name">
                 </div>
 
-                <div class="mb-3">
+                <div>
                     <label for="type" class="form-label">Project Name:</label>
                     <input type="text" class="form-control" id="type" v-model="inventoryElement.project_name">
-                    <p style="margin-top:10%;text-align:center; font-weight: bolder;">You added: {{ inventoryComponents.length }} components</p>
                 </div>
-                <button type="submit" id="submitinventory" class="btn btn-primary">Update Inventory</button>
-             </div>
+                <p class="mt-4" style="text-align:center; font-weight: bolder;">You added: {{ inventoryComponents.length }} components</p>
+                <button type="submit"  class="btn btn-primary">Update Inventory</button>
+                <hr>
+        </div>
     </form>
-        <table class="table-responsive">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th scope="col">Subtype</th>
-                <th scope="col">Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="component in inventoryComponents" :key="component.id">
-                    <td>{{ component.id }}</td>
-                    <td>{{ component.name }}</td>
-                    <td>{{ component.component_type }}</td>
-                    <td>{{ component.component_subtype }}</td>
-                    <td>
-                        <button class="btn btn-danger" style="margin-right:2px;" @click="deleteComponent(component.id)">
-                            Delete From Inventory
-                        </button>
-                    </td>
-            </tr>
-            </tbody>
-        </table>
     </div>
-</div>
-</template>
+  </div>
+  <div v-if="YouHaveAddComponent" class="form-table">
+        <hr>
+        <hr style="color:white; height:5px;">
+      <h4 style="color:green; text-align: center;">Inserted Components:</h4>
+      <table style="margin-top: 20px; max-height: 300px; margin-bottom: 10px; width:50%;" class="table-responsive">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Type</th>
+            <th scope="col">Subtype</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="component in inventoryComponents" :key="component.id">
+                <td>{{ component.id }}</td>
+                <td>{{ component.name }}</td>
+                <td>{{ component.component_type }}</td>
+                <td>{{ component.component_subtype }}</td>
+                <td>
+                    <button class="btn btn-danger" style="margin-right:2px;" @click="deleteComponent(component.id)">
+                        Delete From Inventory
+                    </button>
+                </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  </template>
 
 
 <script>
@@ -193,60 +199,70 @@ export default {
 </script>
 
 <style scoped>
-.tablecontainer{
-
-display:flex;
-justify-content: space-around;
-
-}
+/* Add styles as needed */
+.tablecontainer{   }
 table {
-margin-left: 0.5%;
-border-collapse: collapse;
-border:0.5px solid grey;
+  margin-left: 0.5%;
+  border:0.5px solid grey;
+  max-height: 600px;
 }
 
 th, td {
-border: 1px solid #ddd;
-padding: 6px;
-text-align: center;
-}
-
-th {
-background-color: #f2f2f2;
+  border: 1px solid #ddd;
+  padding: 6px;
+  text-align: center;
+  text-overflow: ellipsis;
+  white-space: normal;
+  font-size: medium;
+  word-wrap: break-word;
+  width:200px;
+  max-width: 200px;
 }
 
 .mb-3{
-    width: 100%;             /* Make each form element take full width on smaller screens */
-    box-sizing: border-box;  /* Include padding and border in the width */
-    margin-left: 0.5%;
+        width: 100%;             /* Make each form element take full width on smaller screens */
+        box-sizing: border-box;  /* Include padding and border in the width */
+        margin-left: 0.5%;
 
-}
-.addedcomponents{
-    margin-top:34px;
-}
+    }
+    .addedcomponents{
+      width:50%;
+      margin-left:20px;
+      height: 400px;
+      margin-right:5%;
+       background-color: aliceblue;
+    }
 
-#component{
-    width:100%;
-    margin-bottom: 5px;
-}
+    #component{}
+      
+    
 
-#submitinventory {
-width: 55%;
-margin-bottom: 10%;
-margin-left: 0%;
-}
-
-.table-responsive {
-display: block;
-width: fit-content;
-overflow-x: auto;
--webkit-overflow-scrolling: touch;
--ms-overflow-style: -ms-autohiding-scrollbar;
-max-height: 400px;
-}
+    #submitinventory {
+    }
 
 #ChooseTechnology{
     margin-top: 10px;
 }
+
+  .table-responsive {
+    margin: 0 auto;
+    display: block;
+    max-height: 400px;
+    overflow-x: auto;
+    -webkit-overflow-scrolling: touch;
+    -ms-overflow-style: -ms-autohiding-scrollbar;
+}
+
+.group1{
+  margin: 0 auto;
+  width:50%;
+}
+
+.test{
+  display: flex;
+  justify-content: flex-start;
+}
+
+
 
 </style>
