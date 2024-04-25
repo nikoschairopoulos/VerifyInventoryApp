97ff23a63d457cbdb2681e1668c6ec68c3066801
nikoschairopoulos <nikoschairop@gmail.com>
2024-04-25 16:30:59 +0300

    script to load the D components

--- InventoryApp/InventoryFrontBackEnd/Import_D_components.py
+++ InventoryApp/InventoryFrontBackEnd/Import_D_components.py
@@ -10,54 +10,81 @@ django.setup()
 
 # Now this script or any imported module can use any part of Django it needs.
 '''
+
+
 import os
 import sys
 import django
-sys.path.append('/home/nikos/Desktop/InventoryApp/InventoryApp/InventoryFrontBackEnd')
-#from django_project import *
+from pathlib import Path
+from django.shortcuts import get_object_or_404
+
+#from folder.subfolder.myfile import tade
+
+if __name__  != "__main__":
+    raise RuntimeError("Do not import from this script")
+
+#add path:
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventoryapi.settings')
 django.setup()
-
-
 from inventory.models import Component
 from inventory.api.serializers import ComponentSerializer
 import pandas as pd 
 
-# This is a test:
+#FILES PATH:
+CURRENT_DIRECTORY = Path(__file__).parent.absolute()
+DATA_FILE = CURRENT_DIRECTORY/'library_new.xlsm'
+
+## NOT ADD SECTION:
+#SUBTYPES_TO_NOT_ADD_PLANTS = {'pemfc','pafc','safc','afc','sofc','mcfc'}
+##
 
 class FillDataBaseWithComponentsD:
-    #print(Component.objects.all())
+
+    def __init__(self):
+        print('loader initialized..')
+        #print(Component.objects.all())
+
     def load_components(self):
-        DATAFRAME_DICT_COMPONENTS = self.read_library('/inventory')
-        for comp in DATAFRAME_DICT_COMPONENTS.values():
-            serializer = ComponentSerializer(data=comp)
-            if serializer.is_valid():
-                new_component = serializer.save()
-            else:
-                print("error:",serializer.errors)
-    
-    @classmethod
-    def read_library(cls,path):
-        sheets = [
-            "Thermal Sources",
-            "Ventilation",
-            "Glazing",
-            "Insulation",
-            "PCM",
-            "Water Storage",
-            "El. Generators",
-            "El. Storage",
-            # "Various Assets",
-            "Rigid Materials",
-            "Fuels",
-            "El. Profiles",
-            # "Plants"
-        ]
-        library = {sheet: cls.read_library_sheet(path, sheet) for sheet in sheets}
-        return library
-    
-    @classmethod
-    def read_library_sheet(cls,path, sheet_name):
+        '''
+        #script_dir = os.path.dirname(os.path.abspath(__file__))
+        #file_path = os.path.join(script_dir,'library_new.xlsm')
+        '''
+        DATAFRAME_DICT_COMPONENTS = FillDataBaseWithComponentsD.read_library( DATA_FILE )
+        for SHEET_TYPE, dataframe_of_sheet_type in DATAFRAME_DICT_COMPONENTS.items():
+            #print(comp)
+            for index, row in dataframe_of_sheet_type.iterrows():
+                comp_dict = row.to_dict()
+            
+                #Add name of the component:
+                component_name = f"{comp_dict['type']}_{comp_dict['subtype']} (default)"
+               
+                #DO SOME RENAMINGS:
+                comp_dict['name'] = component_name
+                comp_dict['component_type'] = comp_dict.pop('type')
+                comp_dict['component_subtype'] = comp_dict.pop('subtype')
+                comp_dict['capex_per_ugs'] = comp_dict.pop('capex/u.g.s.')
+                comp_dict['embodied_co2_per_ugs'] = comp_dict.pop('embodied_co2/u.g.s.')
+                comp_dict['embodied_pe_per_ugs'] = comp_dict.pop('embodied_pe/u.g.s.')
+                comp_dict['pref_cost'] = comp_dict.pop('Pref_cost')
+                comp_dict['pref_env'] = comp_dict.pop('Pref_env')
+                comp_dict['SHEET_TYPE'] = SHEET_TYPE
+                comp_dict['IS_MAIN_INVENTORY'] = True
+                comp_dict['replace_or_die'] = 'replace'
+                #breakpoint()
+
+                #SERIALIZE AND LOAD:
+                serializer = ComponentSerializer(data=comp_dict)
+                if serializer.is_valid():
+                    new_component = serializer.save()
+                    #breakpoint()
+                else:
+                    print("error:",serializer.errors)
+        
+###################
+#Methods from Verify
+###################
+    @staticmethod
+    def read_library_sheet(path, sheet_name):
         df = pd.read_excel(path, sheet_name=sheet_name, skiprows=1)
         df = df.iloc[:, 1:]
         df = df[df.columns[~df.columns.isin(["source"])]]
@@ -71,3 +98,48 @@ class FillDataBaseWithComponentsD:
         # print(df)
         return df
 
+    @staticmethod
+    def read_library(path):
+        sheets = [
+            #"Thermal Sources",
+            #"Ventilation",
+            #"Glazing",
+            #"Insulation",
+            #"PCM",
+            #"Water Storage",
+            #"El. Generators",
+            #"El. Storage",
+            #"Various Assets",
+            #"Rigid Materials",
+            #"Fuels",
+            #"El. Profiles",
+            "Plants",
+            "Transport",
+            "Public"
+        ]
+        library = {sheet: FillDataBaseWithComponentsD.read_library_sheet(path, sheet) for sheet in sheets}
+        return library
+
+
+
+
+# Iterate over each path in sys.path and print it
+    #for path in sys.path:
+    #    print(path)
+
+#obj = FillDataBaseWithComponentsD()
+#obj.load_components()
+
+#update_name = {component_name="this is a new name"}
+def update_component(primary_key,update_name):
+    obj = get_object_or_404(Component,pk=primary_key)
+    data_of_object = ComponentSerializer(obj).data
+    del data_of_object['name']
+    data_of_object['name'] =  update_name
+    serializer = ComponentSerializer(instance=obj,data=data_of_object)
+    if serializer.is_valid():
+        serializer.save()
+    else:
+        print(serializer.errors)
+
+update_component(338,'update_name')
--- InventoryApp/InventoryFrontBackEnd/inventory/models.py
+++ InventoryApp/InventoryFrontBackEnd/inventory/models.py
@@ -36,6 +36,7 @@ class Component(models.Model):
     bibliography = models.TextField(blank=True,null=True)
     description = models.TextField(blank=True,null=True)
     thermal_properties = models.JSONField(null=True, blank=True)
+    IS_B_COMPONENT = models.BooleanField(null=True)
 
     class Meta:
         app_label = 'inventory'
