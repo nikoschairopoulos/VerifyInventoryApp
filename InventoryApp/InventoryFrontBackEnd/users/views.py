5fe9aad7831ac47490b3923add5898527a9bead6
nikos <nikoschairop@gmail.com>
2024-10-31 13:54:42 +0200

    Make User - Inventory creation ROLL BACK

--- InventoryApp/InventoryFrontBackEnd/users/views.py
+++ InventoryApp/InventoryFrontBackEnd/users/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from .models import CustomUser
 from inventory.models import Inventory
 from rest_framework.response import Response
+from rest_framework import serializers
+from django.db import transaction
 from .conf import (WORD,KEY)
 
 class CreateUser(APIView):
@@ -11,34 +13,39 @@ class CreateUser(APIView):
         #take project name
         project_name = data['user_email'].split('@')[0]
         
-        arguments = {
-            'is_superuser':False,
-            'username':f'user_{project_name}',
-            'first_name':'',
-            'last_name':'',
-            'email':data['user_email'],
-            'is_staff':False,
-            'is_active':True
-        }
-        #create user:
-        user_to_add = CustomUser(
-            **arguments
-        )
+        try:
+            with transaction.atomic():  # Start a transaction
+                arguments = {
+                    'is_superuser':False,
+                    'username':f'user_{project_name}',
+                    'first_name':'',
+                    'last_name':'',
+                    'email':data['user_email'],
+                    'is_staff':False,
+                    'is_active':True
+                }
+                #create user:
+                user_to_add = CustomUser(
+                    **arguments
+                )
 
-        #hash the password:
-        user_to_add.set_password(f'{WORD}_{project_name}')
-        user_to_add.save()
+                #hash the password:
+                user_to_add.set_password(f'{WORD}_{project_name}')
+                user_to_add.save()
 
 
-        #create a blank inventory for this user:
-        new_inventory = Inventory(**{
-            'author':user_to_add,
-            'name':'custom_invnentory: '+f'{project_name}',
-            'project_name':'project_name'
-        })
-        new_inventory.save()
+                #create a blank inventory for this user:
+                new_inventory = Inventory(**{
+                    'author':user_to_add,
+                    'name':'custom_invnentory: '+f'{project_name}',
+                    'project_name':'project_name'
+                })
+                new_inventory.save()
 
-        return Response({'message':f'new LCI user have been created with emai: {arguments["email"]}'})
+                return Response({'message':f'new LCI user have been created with emai: {arguments["email"]}'})
+        except Exception as e:
+            print(e)
+            raise serializers.ValidationError({'error': str(e)})
         
 
-# Create your views here.
+
